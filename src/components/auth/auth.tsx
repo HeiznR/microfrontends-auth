97c063164be513d53d3cf9f555ed18784dc51ad3
LilHeizy <vishnyakov98@gmail.com>
2025-04-02 21:27:16 +0200

    microfrontend: remote auth app init

---
+++ src/components/auth/auth.tsx
@@ -0,0 +1,12 @@
+const LoginForm = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </div>
+  );
+};
+
+export default LoginForm;
